c725ea901377aa3a67d086a0d097ff521e588eed
a.skorik <a.skorik@petrovich.ru>
2023-09-30 19:39:50 +0300

    added conftes.py, requirements.txt, test_automation_practice_form.py

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+"""фикстуры используемые для тестирования формы
+https://demoqa.com/automation-practice-form"""
+import pytest
+from selene.support.shared import browser
+
+
+@pytest.fixture(scope='function')
+def browser_setup():
+    """Фикстура настройки браузера"""
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+
+
+@pytest.fixture(scope='function')
+def open_url(browser_setup):
+    """Фикстура открытия браузера с указанным url"""
+    browser.open('/')
+    yield browser
+    browser.quit()
---
+++ tests/test_automation_practice_form.py
@@ -0,0 +1,6 @@
+"""автотест на заполнение и отправку формы
+https://demoqa.com/automation-practice-form """
+
+
+def test_filling_and_send_form(open_url):
+    pass
