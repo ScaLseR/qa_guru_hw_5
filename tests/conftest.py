67ebb601498d36200a9fcbfd90288bcc12a03a70
a.skorik <a.skorik@petrovich.ru>
2023-10-02 08:54:01 +0300

    edited test_automation_practice_form.py, conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,18 +4,11 @@ import pytest
 from selene.support.shared import browser
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='function', autouse=True)
 def browser_setup():
     """Фикстура настройки браузера"""
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.config.timeout = 2.0
-    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+    browser.config.base_url = 'https://demoqa.com'
 
-
-@pytest.fixture(scope='function')
-def open_url(browser_setup):
-    """Фикстура открытия браузера с указанным url"""
-    browser.open('/')
-    yield browser
-    browser.quit()
--- tests/test_automation_practice_form.py
+++ tests/test_automation_practice_form.py
@@ -2,34 +2,37 @@
 https://demoqa.com/automation-practice-form """
 from selene import have
 from os import path
+from selene.support.shared import browser
 
 
-def test_filling_and_send_form(open_url):
-    open_url.element('#firstName').type('Бобр')
-    open_url.element('#lastName').type('Добр')
-    open_url.element('#userEmail').type('bobrdobr@test.ru')
-    open_url.element('[for="gender-radio-1"]').click()
-    open_url.element('#userNumber').type('1234567890')
-    open_url.element('#dateOfBirthInput').click()
-    open_url.element('.react-datepicker__month-select').type('May')
-    open_url.element('.react-datepicker__year-select').type('2022')
-    open_url.element('.react-datepicker__day--012').click()
-    open_url.element('#subjectsInput').type('English').press_enter().type('Computer Science').press_enter()
-    open_url.element('[for="hobbies-checkbox-2"]').click()
-    open_url.element("#uploadPicture").send_keys(path.abspath('bobr.jpg'))
-    open_url.element('#currentAddress').type('Бобриное Болото')
-    open_url.element('#react-select-3-input').type('Haryana').press_enter()
-    open_url.element('#react-select-4-input').type('Karnal').press_enter()
-    open_url.element('#submit').click()
+def test_filling_and_send_form():
+    browser.open('/automation-practice-form')
 
-    open_url.element("#example-modal-sizes-title-lg").should(have.text("Thanks for submitting the form"))
-    open_url.element('.table').all('tr td:nth-child(2)').should(have.texts('Бобр Добр',
-                                                                           'bobrdobr@test.ru',
-                                                                           'Male',
-                                                                           '1234567890',
-                                                                           '12 May,2022',
-                                                                           'English, Computer Science',
-                                                                           'Reading',
-                                                                           'bobr.jpg',
-                                                                           'Бобриное Болото',
-                                                                           'Haryana Karnal'))
+    browser.element('#firstName').type('Бобр')
+    browser.element('#lastName').type('Добр')
+    browser.element('#userEmail').type('bobrdobr@test.ru')
+    browser.element('[for="gender-radio-1"]').click()
+    browser.element('#userNumber').type('1234567890')
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__month-select').type('May')
+    browser.element('.react-datepicker__year-select').type('2022')
+    browser.element('.react-datepicker__day--012').click()
+    browser.element('#subjectsInput').type('English').press_enter().type('Computer Science').press_enter()
+    browser.element('[for="hobbies-checkbox-2"]').click()
+    browser.element("#uploadPicture").send_keys(path.abspath('bobr.jpg'))
+    browser.element('#currentAddress').type('Бобриное Болото')
+    browser.element('#react-select-3-input').type('Haryana').press_enter()
+    browser.element('#react-select-4-input').type('Karnal').press_enter()
+    browser.element('#submit').click()
+
+    browser.element("#example-modal-sizes-title-lg").should(have.text("Thanks for submitting the form"))
+    browser.element('.table').all('tr td:nth-child(2)').should(have.texts('Бобр Добр',
+                                                                      'bobrdobr@test.ru',
+                                                                      'Male',
+                                                                      '1234567890',
+                                                                      '12 May,2022',
+                                                                      'English, Computer Science',
+                                                                      'Reading',
+                                                                      'bobr.jpg',
+                                                                      'Бобриное Болото',
+                                                                      'Haryana Karnal'))
